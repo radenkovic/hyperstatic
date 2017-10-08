255334560414329d42880b1ac1de53d2b19c0b40
Dan Radenkovic <bogdan.radenkovic@gmail.com>
2017-10-08 14:12:05 +0200

    fix paths

--- gulpfile.js
+++ gulpfile.js
@@ -35,56 +35,56 @@ gulp.task('serve', ['sass', 'js', 'nunjucks', 'copyImages', 'references'], () =>
 });
 
 gulp.task('sass', () => {
-  return gulp.src("src/scss/**/*.scss")
+  return gulp.src("./src/scss/**/*.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('style.css'))
-    .pipe(gulp.dest("src/public/assets"))
+    .pipe(gulp.dest("./src/public/assets"))
     .pipe(browserSync.stream());
 });
 
 gulp.task('js', () => {
-  return gulp.src("src/js/**/*.js")
+  return gulp.src("./src/js/**/*.js")
     .pipe(concat('bundle.js'))
-    .pipe(gulp.dest("src/public/assets"))
+    .pipe(gulp.dest("./src/public/assets"))
     .pipe(browserSync.stream());
 })
 
 gulp.task('nunjucks', function() {
-  return gulp.src('src/templates/*.html')
+  return gulp.src('./src/templates/*.html')
     .pipe(nunjucks({searchPaths: ['src/templates']}))
-    .pipe(gulp.dest('src/public/'));
+    .pipe(gulp.dest('./src/public/'));
 });
 
 gulp.task('copyImages', () => {
-    return gulp.src('src/images/**/*')
-      .pipe(gulp.dest('src/public/assets/images'))
+    return gulp.src('./src/images/**/*')
+      .pipe(gulp.dest('./src/public/assets/images'))
 });
 
 gulp.task('images', () => {
-    return gulp.src('src/images/**/*')
+    return gulp.src('./src/images/**/*')
       .pipe(imagemin())
-      .pipe(gulp.dest('src/public/assets/images'))
+      .pipe(gulp.dest('./src/public/assets/images'))
 });
 
 gulp.task('references', () => {
-    return gulp.src('src/references/**/*')
-      .pipe(gulp.dest('src/public/references'))
+    return gulp.src('./src/references/**/*')
+      .pipe(gulp.dest('./src/public/references'))
 });
 
 gulp.task('copyReferences', () => {
-    return gulp.src('src/references/**/*')
-      .pipe(gulp.dest('dist/references'))
+    return gulp.src('./src/references/**/*')
+      .pipe(gulp.dest('./dist/references'))
 });
 
 gulp.task('docs', () => {
-  return gulp.src('dist/*')
-    .pipe(gulp.dest('docs'))
+  return gulp.src('./dist/*')
+    .pipe(gulp.dest('./docs'))
 })
 
 // BUILD TASK
 gulp.task('build', ['sass', 'js', 'nunjucks', 'images', 'copyReferences'], (done) => {
   // References
-  const items = fs.readdirSync('src/public/references');
+  const items = fs.readdirSync('./src/public/references');
   const IGNORED_FILES = items.map(item => `references/${item}`)
 
   glob('./src/public/' + '*.html', (err, files) => {
@@ -98,7 +98,7 @@ gulp.task('build', ['sass', 'js', 'nunjucks', 'images', 'copyReferences'], (done
          ignore: IGNORED_FILES
        }))
        .pipe(htmlmin({collapseWhitespace: true}))
-       .pipe(gulp.dest('dist'));
+       .pipe(gulp.dest('./dist'));
      });
      eventStream.merge(tasks).on('end', done);
  });
